14bdd12869b96a814a819d5a3e469cf30a9eeb69
momus <momus300@gmail.com>
2018-05-17 22:49:02 +0200

    proste api

--- client.js
+++ client.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const app = express();
+const morgan = require('morgan');
 
 const productRoutes = require('./api/routes/products');
+
+app.use(morgan('dev'));
 
 app.use('/public', productRoutes);
 
