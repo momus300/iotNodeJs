9c69c8929a27a40bf5dc2dbb519976e461c3f003
momus <momus300@gmail.com>
2018-05-22 21:27:15 +0200

    proste api

--- server.js
+++ server.js
@@ -1,9 +1,32 @@
-const http = require('http');
+// const http = require('http');
+//
+// const app = require('./app');
+//
+// const port = process.env.PORT || 8081;
+//
+// const server = http.createServer(app);
+//
+// server.listen(port);
 
-const app = require('./app');
+
+
+
+
+
+
+////////////////
+
+
+
+
+const http = require('http');
 
 const port = process.env.PORT || 8081;
 
-const server = http.createServer(app);
+const server = http.createServer(function (req, res) {
+    res.writeHead(200, {'Content-Type': 'text/plain'});
+    res.write('Hello World!');
+    res.end();
+});
 
 server.listen(port);
