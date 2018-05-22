2b13e2bc5a270df8b040ff10dcc7538f824c2d93
momus <momus300@gmail.com>
2018-05-22 21:35:14 +0200

    back

--- server.js
+++ server.js
@@ -1,12 +1,12 @@
-// const http = require('http');
-//
-// const app = require('./app');
-//
-// const port = process.env.PORT || 8081;
-//
-// const server = http.createServer(app);
-//
-// server.listen(port);
+const http = require('http');
+
+const app = require('./app');
+
+const port = process.env.PORT || 8081;
+
+const server = http.createServer(app);
+
+server.listen(port);
 
 
 
@@ -19,14 +19,14 @@
 
 
 
-const http = require('http');
-
-const port = process.env.PORT || 8081;
-
-const server = http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.write('Hello World!');
-    res.end();
-});
-
-server.listen(port);
+// const http = require('http');
+//
+// const port = process.env.PORT || 8081;
+//
+// const server = http.createServer(function (req, res) {
+//     res.writeHead(200, {'Content-Type': 'text/plain'});
+//     res.write('Hello World!');
+//     res.end();
+// });
+//
+// server.listen(port);
